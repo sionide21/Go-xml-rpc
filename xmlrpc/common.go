ce9af5c209ed6f6dc5097f4f47ddfa0daa6371c9
Ben Olive <ben.olive@gatech.edu>
2009-12-15 13:14:37 -0500

    Added Marshall Methods

--- xmlrpc/common.go
+++ xmlrpc/common.go
@@ -1,19 +1,19 @@
 package xmlrpc
 
-import ("time")
+import ("time"; "fmt"; "encoding/base64")
 
 
 // Make fault fit os.Error
 func (f Fault) String() string {
-    return f.faultString
+    return f.FaultString
 }
 
 // The interface that all values that want to be transmitted must conform to.
 type MarshallUnmarshaller interface {
     // Get trasmittable value of this item
-    func Marshall() string;
+    Marshall() string;
     // Populate this item based on the provided value
-    func Unmarshall(string);
+    Unmarshall(string);
 }
 
 // Simple types
@@ -28,6 +28,51 @@ type ArrayValue []MarshallUnmarshaller;
 
 // An error returned from the rpc server
 type Fault struct {
-    faultCode int;
-    faultString string;
+    FaultCode int;
+    FaultString string;
+}
+
+// Marshal functions
+func (i IntValue) Marshall() string {
+    return fmt.Sprintf("<int>%v</int>", i)
+}
+
+func (b BooleanValue) Marshall() string {
+    return fmt.Sprintf("<boolean>%v</boolean>", b)
+}
+func (s StringValue) Marshall() string {
+    return fmt.Sprintf("<string>%v</string>", s)
+}
+func (d DoubleValue) Marshall() string {
+    return fmt.Sprintf("<double>%v</double>", d)
+}
+func (d DateTimeValue) Marshall() string {
+    // TODO try to get ISO8601 in stdlib
+    return fmt.Sprintf("<dateTime.iso8601>%s</dateTime.iso8601>", "NOT IMPLEMENTED")
+}
+func (b Base64Value) Marshall() string {
+    encLen := base64.StdEncoding.EncodedLen(len(b));
+    enc := make([]byte, encLen);
+    base64.StdEncoding.Encode(enc, b);
+    return fmt.Sprintf("<base64>%s</base64>", string(enc));
+}
+func (s StructValue) Marshall() (ret string) {
+    ret = "<struct>";
+    for key, value := range s {
+        ret += fmt.Sprintf("<member><name>%s</name><value>%s</value></member>", key, value.Marshall())
+    }
+    ret += "</struct>";
+    return
+}
+func (s ArrayValue) Marshall() (ret string) {
+    ret = "<array><data>";
+    for _, value := range s {
+        ret += fmt.Sprintf("<value>%s</value>", value.Marshall())
+    }
+    ret += "</data></array>";
+    return
+}
+func (f Fault) Marshall() string {
+    faultStruct := StructValue{"faultCode": IntValue(f.FaultCode), "faultString": StringValue(f.FaultString)};
+    return fmt.Sprintf("<fault>%s</fault>", faultStruct.Marshall())
 }
