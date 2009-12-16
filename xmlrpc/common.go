efeedf9f99e2a0e7db3fa8f6785a443119022f14
Ben Olive <ben.olive@gatech.edu>
2009-12-16 10:36:16 -0500

    Added some Unmarshall methods

--- xmlrpc/common.go
+++ xmlrpc/common.go
@@ -1,19 +1,13 @@
 package xmlrpc
 
-import ("time"; "fmt"; "encoding/base64")
-
-
-// Make fault fit os.Error
-func (f Fault) String() string {
-    return f.FaultString
-}
+import ("time"; "fmt"; "encoding/base64";  "strconv"; "strings"; "xml"; "os")
 
 // The interface that all values that want to be transmitted must conform to.
 type MarshallUnmarshaller interface {
     // Get trasmittable value of this item
     Marshall() string;
     // Populate this item based on the provided value
-    Unmarshall(string);
+    Unmarshall(string) os.Error;
 }
 
 // Simple types
@@ -32,30 +26,88 @@ type Fault struct {
     FaultString string;
 }
 
-// Marshal functions
+// Make fault fit os.Error
+func (f Fault) String() string {
+    return f.FaultString
+}
+
+type error string;
+
+func (e error) String() string {
+    return string(e)
+}
+
+// Marshal and Unmarshall functions
 func (i IntValue) Marshall() string {
     return fmt.Sprintf("<int>%v</int>", i)
+}
+
+func (i IntValue) Unmarshall(s string) (err os.Error) {
+    var tempInt int;
+    tempInt, err = strconv.Atoi(s);
+    i = IntValue(tempInt);
+    return
 }
 
 func (b BooleanValue) Marshall() string {
     return fmt.Sprintf("<boolean>%v</boolean>", b)
 }
+
+func (b BooleanValue) Unmarshall(s string) os.Error {
+    switch s {
+        case "0":
+            b = BooleanValue(false)
+        case "1":
+            b = BooleanValue(true)
+        default:
+            return error(fmt.Sprintf("Unrecognized boolean: %s", s))
+    }
+    return nil;
+}
+
 func (s StringValue) Marshall() string {
     return fmt.Sprintf("<string>%v</string>", s)
 }
+
+func (s StringValue) Unmarshall(val string) os.Error {
+    s = StringValue(val);
+    return nil;
+}
+
 func (d DoubleValue) Marshall() string {
     return fmt.Sprintf("<double>%v</double>", d)
 }
+
+func (d DoubleValue) Unmarshall(val string) (err os.Error) {
+    var tempDouble float;
+    tempDouble, err = strconv.Atof(val);
+    d = DoubleValue(tempDouble);
+    return
+}
+
 func (d DateTimeValue) Marshall() string {
     // TODO try to get ISO8601 in stdlib
     return fmt.Sprintf("<dateTime.iso8601>%s</dateTime.iso8601>", "NOT IMPLEMENTED")
 }
+
+func (d DateTimeValue) Unmarshall(val string) (err os.Error) {
+    return error("date Not Implemented")
+}
+
 func (b Base64Value) Marshall() string {
     encLen := base64.StdEncoding.EncodedLen(len(b));
     enc := make([]byte, encLen);
     base64.StdEncoding.Encode(enc, b);
     return fmt.Sprintf("<base64>%s</base64>", string(enc));
 }
+
+func (b Base64Value) Unmarshall(s string) (err os.Error) {
+    decLen := base64.StdEncoding.DecodedLen(len(s));
+    b = Base64Value(make([]byte, decLen));
+    _,err = base64.StdEncoding.Decode(b, strings.Bytes(s));
+    return
+}
+
 func (s StructValue) Marshall() (ret string) {
     ret = "<struct>";
     for key, value := range s {
