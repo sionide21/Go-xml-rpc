a811e295c160802a8815c269ae66324891b6e368
Ben Olive <ben.olive@gatech.edu>
2009-12-20 23:52:58 -0500

    Call now returns a ParamValue

--- xmlrpc/http.go
+++ xmlrpc/http.go
@@ -9,12 +9,13 @@ import (
 // Calls a remote xmlrpc method. Response is the valid response form the server.
 // Error may either be a local error or a remote one. If it is remote it will be of type
 // xmlrpc.Fault.
-func (r RemoteMethod) Call(args ...) (Response, os.Error) {
+func (r RemoteMethod) Call(args ...) (ParamValue, os.Error) {
 	body := new(bytes.Buffer)
 	r.SendXML(body, Params(args))
 	resp, err := http.Post(r.Endpoint, "text/xml", body)
 	if err != nil {
-		return Response{}, err
+		return nil, err
 	}
-	return ReadResponse(resp.Body)
+	ret, err := ReadResponse(resp.Body)
+	return ret.Value, err
 }
