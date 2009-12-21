8941da4eee2b7b618607838e034548abf74c5be2
Ben Olive <ben.olive@gatech.edu>
2009-12-20 23:33:23 -0500

    Changed the way requests are made.
    New way is much cleaner

--- xmlrpc/http.go
+++ xmlrpc/http.go
@@ -9,10 +9,10 @@ import (
 // Calls a remote xmlrpc method. Response is the valid response form the server.
 // Error may either be a local error or a remote one. If it is remote it will be of type
 // xmlrpc.Fault.
-func Call(endpoint string, req Request) (Response, os.Error) {
+func (r RemoteMethod) Call(args ...) (Response, os.Error) {
 	body := new(bytes.Buffer)
-	req.SendXML(body)
-	resp, err := http.Post(endpoint, "text/xml", body)
+	r.SendXML(body, Params(args))
+	resp, err := http.Post(r.Endpoint, "text/xml", body)
 	if err != nil {
 		return Response{}, err
 	}
--- xmlrpc/request.go
+++ xmlrpc/request.go
@@ -5,23 +5,23 @@ import (
 	"fmt"
 )
 
-// The remote method to call. Method is the name of the method.
-// Params is the list of arguments to pass to `Method`.
-type Request struct {
-	Method string
-	Params []ParamValue
+// The remote method to call. Endpoint is the URL of the endpoint
+// Method is the name of the method to call.
+type RemoteMethod struct {
+	Endpoint string
+	Method   string
 }
 
 // This methods writes the xml representation of the request
 // to a io.Writer. It is public mostly for debugging or alternate
 // communication. To call a remote function you should use
 // `xmlrpc.Call`.
-func (r Request) SendXML(out io.Writer) {
+func (r RemoteMethod) SendXML(out io.Writer, params []ParamValue) {
 	io.WriteString(out, "<?xml version=\"1.0\"?>\n")
 	io.WriteString(out, "<methodCall>")
 	io.WriteString(out, fmt.Sprintf("<methodName>%s</methodName>", r.Method))
 	io.WriteString(out, "<params>")
-	for _, p := range r.Params {
+	for _, p := range params {
 		io.WriteString(out, "<param><value>")
 		io.WriteString(out, p.ToXML())
 		io.WriteString(out, "</value></param>")
