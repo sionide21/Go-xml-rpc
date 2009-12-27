6d24bc7f54a6defc31df573bdc3c6d9cabe0d1d7
Ben Olive <ben.olive@gatech.edu>
2009-12-27 17:04:37 -0500

    Added call methods with specific return types for ease of use

--- xmlrpc/http.go
+++ xmlrpc/http.go
@@ -8,6 +8,7 @@ import (
 	"http"
 	"bytes"
 	"os"
+	"time"
 )
 
 // Calls a remote xmlrpc method. Response is the valid response form the server.
@@ -22,4 +23,68 @@ func (r RemoteMethod) Call(args ...) (ParamValue, os.Error) {
 	}
 	ret, err := ReadResponse(resp.Body)
 	return ret.Value, err
+}
+
+func (r RemoteMethod) CallInt(args ...) (int, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return 0, err
+	}
+	return int(res.(IntValue)), nil
+}
+
+func (r RemoteMethod) CallBoolean(args ...) (bool, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return false, err
+	}
+	return bool(res.(BooleanValue)), nil
+}
+
+func (r RemoteMethod) CallDouble(args ...) (float, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return 0, err
+	}
+	return float(res.(DoubleValue)), nil
+}
+
+func (r RemoteMethod) CallDate(args ...) (time.Time, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return time.Time(res.(DateTimeValue)), nil
+}
+
+func (r RemoteMethod) CallBytes(args ...) ([]byte, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(res.(Base64Value)), nil
+}
+
+func (r RemoteMethod) CallStruct(args ...) (StructValue, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return nil, err
+	}
+	return res.(StructValue), nil
+}
+
+func (r RemoteMethod) CallArray(args ...) (ArrayValue, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return nil, err
+	}
+	return res.(ArrayValue), nil
+}
+
+func (r RemoteMethod) CallString(args ...) (string, os.Error) {
+	res, err := r.Call(args)
+	if err != nil {
+		return "", err
+	}
+	return string(res.(StringValue)), nil
 }
